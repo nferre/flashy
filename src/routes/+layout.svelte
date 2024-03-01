0b7d332ede48b781a42dc9bafede0feef95e474f
nferre <nferre@student.42nice.fr>
2024-03-01 18:43:22 +0100

    fixed redirection

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,5 @@
 <script>
     import "../app.css"
-    import {goto} from '$app/navigation'
-    import { page } from '$app/stores';
-
-    $: if($page.route.id == "/") {
-        goto('/flashy/flashy');
-    }
 </script>
 
 <div class={'min-h-screen bg-yellow-50'}>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+    import { browser } from '$app/environment';
+    if (browser) { // to prevent error window is not defined, because it's SSR
+        window.location.href = '/flashy/flashy';
+    }
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
