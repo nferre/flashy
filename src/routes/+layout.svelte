041c451b322e5d5100fb7638258b71d28d855bb5
nferre <nferre@student.42nice.fr>
2024-03-01 16:40:01 +0100

    redirect / to /flashy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
     import "../app.css"
+    import {goto} from '$app/navigation'
+    import { page } from '$app/stores';
+
+    $: if($page.route.id == "/") {
+        goto('/flashy/flashy');
+    }
 </script>
 
 <div class={'min-h-screen bg-yellow-50'}>
